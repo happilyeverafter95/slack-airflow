d27775d755fa1e56733bf3783c67098122c8af9d
happilyeverafter95 <mgu@wealthsimple.com>
2020-06-07 11:43:19 -0400

    use class

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -2,7 +2,6 @@ import os
 import requests
 
 from airflow import DAG
-from airflow.operators import PythonOperator
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 
 from datetime import datetime
@@ -15,14 +14,18 @@ default_args = {
     'retries': 0,
 }
 
-daily_weather = None
+
+class DailyForecast:
+    def __init__(self):
+        self.forecast = self.get_daily_forecast()
+
+    def get_daily_forecast(self) -> str:
+        api_key = os.environ['weather_api_key']
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID={}'.format(api_key))
+        return r.json()['weather'][0]['description']
 
 
-def get_daily_temperature():
-    api_key = os.environ['weather_api_key']
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID={}'.format(api_key))
-    global daily_weather
-    daily_weather = r.json()['weather'][0]['description']
+daily_forecast = DailyForecast()
 
 
 with DAG(
@@ -31,16 +34,10 @@ with DAG(
     schedule_interval='12 * * * *',
     catchup=False,
 ) as dag:
-    get_toronto_temperature = PythonOperator(
-        task_id='get_toronto_temperature',
-        python_callable=get_daily_temperature
-    )
     post_to_slack = SlackWebhookOperator(
         task_id='post_to_slack',
         http_conn_id='slack_connection',
         webhook_token=os.environ['slack_webhook_url'],
-        message='The weather today is {}'.format(daily_weather),
+        message='The forecast today is: {}'.format(daily_forecast.forecast),
         channel='#daily-weather-feed'
     )
-
-    get_toronto_temperature.set_downstream(post_to_slack)
